ec0e8aee5960e0a95b4fdc8a64a1618b4cb6faa7
luis soto <luisinformatica.89@gmail.com>
2022-01-03 17:50:33 -0500

    local storage

---
+++ MODULO_2/07-Objetos/script.js
@@ -0,0 +1,37 @@
+// mostrar el obtener localStorage al momento de cargar la pagina cargar
+// o el dom content loader
+
+document.addEventListener('DOMContentLoaded', function () {
+    obtenerlocalstorage();
+})
+
+
+// capturar los datos del formulario
+function capturardatos() {
+
+    let nombre = document.getElementById('nombre').value;
+    let apellido = document.getElementById('apellido').value;
+    let telefono = document.getElementById('telefono').value;
+
+    // crear un objeto para almacenar los valores al tiempo
+
+    let objeto = {
+        nom: nombre,
+        apel: apellido,
+        tel: telefono,
+
+
+    }
+
+
+    localStorage.setItem('persona', JSON.stringify(objeto));
+    //console aplication localStorage
+}
+
+// OBTENER LOCAL STORAGE 
+
+function obtenerlocalstorage() {
+    let objeto = localStorage.getItem('persona');
+    console.log(objeto);
+    alert(objeto);
+}
